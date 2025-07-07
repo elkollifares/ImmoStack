972fbd31f60ece8199f2eac3979da0bf56c65b31
Fares el Kolli <el_kolli.fares@courrier.uqam.ca>
2025-07-06 22:29:02 -0400

    Mise à jour des configurations FIREBASE

--- frontend/immoquebec/src/app/environments/environment.prod.ts
+++ frontend/immoquebec/src/app/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "REMOVED_GOOGLE_KEY",
-    authDomain: "REMOVED_Auth_Domain",
-    projectId: "REMOVED_PROJECT_ID",
-    storageBucket: "REMOVED_Storage",
-    messagingSenderId: "REMOVED_SENDER_ID",
-    appId: "REMOVED_Sender",
-    measurementId: "REMOVED_MEASUREMENT_ID"
+    apiKey: process.env['FIREBASE_API_KEY'],
+    authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
+    projectId: process.env['FIREBASE_PROJECT_ID'],
+    storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
+    messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'],
+    appId: process.env['FIREBASE_APP_ID'],
+    measurementId: process.env['FIREBASE_MEASUREMENT_ID'],
   }
 };
